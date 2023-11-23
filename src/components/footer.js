69f13616428b59899d467e9f0b012d680494e3db
Simone Egger <simone.egger@mercedes-benz.com>
2023-11-23 11:45:00 +0100

    feat(footer): add imprint and data privacy

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,31 @@ const Footer = () => {
   return (
     <footer className="mt-16 bg-neutral-100 py-8 text-neutral-700">
       <div className="container">
-        <a href="https://authentischsein-coaching.de/">
+        <a
+          href="https://authentischsein-coaching.de/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-3"
+        >
           Copyright {currentYear} Jessica Seidel
+        </a>
+        <span>|</span>
+        <a
+          className="ml-3 mr-3"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://authentischsein-coaching.de/impressum"
+        >
+          Impressum
+        </a>
+        <span>|</span>
+        <a
+          className="ml-3 mr-3"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://authentischsein-coaching.de/datenschutz"
+        >
+          Datenschutz
         </a>
       </div>
     </footer>
